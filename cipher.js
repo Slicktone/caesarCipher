a4ce0b994552439d4d8dc044d894959f090e925b
Slicktone <tclaytony23@gmail.com>
2022-04-07 15:32:46 -0700

    logic for lowercase

--- cipher.js
+++ cipher.js
@@ -1,13 +1,23 @@
 
 //handling the logic for A-Z (65-90 in ASCII) for capital
-//figure out how to handle n
+//figure out how to handle n above
 char = char.charCodeAt(0);
 if (char >= 65 && char <= 90) {
     char += n;
-}
-if (char > 90) {
+    if (char > 90) {
     char -= 26;
-}
-else if (char < 65) {
+ } else if (char < 65) {
     char += 26;
+ }
+}
+//logic for lowercase a - z (97-122)
+else if (char >= 97 && char <= 90) {
+    char += n;
+
+if (char > 122) {
+    char -= 26;
+ }
+else if (char > 122) {
+    char += 26;
+ }
 }
