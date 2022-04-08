be44b4ad5fc70e26e16b343be6fb702b80bdfc46
Slicktone <tclaytony23@gmail.com>
2022-04-08 12:01:38 -0700

    trying a for loop

--- cipher.js
+++ cipher.js
@@ -1,11 +1,19 @@
 function caesarCipher (s, k) {
     //string s = cleartext
-    //int k = alphabet rotation factor
+    //int k = alphabet rotation factor (shift)
 
     let n = k % 26;
     let message = ""
 
+// try a for loop with an empty array
+    let resultArray = []
+    for (let i = 0; i < String.length; i++) {
+        // identify the ASCII code using charCodeAt
+        let code = string.charCodeAt(i) + k
+    }
+}
 
+    /*
     //handling the logic for A-Z (65-90 in ASCII) for capital
     char = char.charCodeAt(0);
     if (char >= 65 && char <= 90) {
@@ -31,3 +39,4 @@ function caesarCipher (s, k) {
     
     return message
 }
+*/
